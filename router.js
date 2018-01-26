60c40b7d8933a5eedbc0432cba3699702ab05f68
Zsjdar <214755651@qq.com>
2018-01-26 19:22:35 +0800

    设计路由表&提取多个处理函数模块

--- router.js
+++ router.js
@@ -1,11 +1,32 @@
 //0.加载express
 const express = require('express')
+//加载所有处理函数模块
+const index = require('./controllers/index')
+const topic = require('./controllers/topic')
+const user = require('./controllers/user')
 //1.调用express.Router()创建一个路由实例
 const router = express.Router()
 //2.配置路由规则
-router.get('/',(req,res)=>{
-	res.send('index page')
-})
+
+//首页路由
+router
+   .get('/',index.showIndex)
+//用户路由
+router
+   .get('/signin',user.showSignin)
+   .post('/signin',user.signin)
+   .get('/signup',user.showSignup) 
+   .post('/signup',user.signup) 
+   .post('/signout',user.signout) 
+ //话题相关
+ router 
+   .get('/topic/creat',topic.showCreate)
+   .post('/topic/creat',topic.create)
+   .get('/topic/:topicID',topic.show)
+   .get('/topic/:topicID/edit',topic.showEdit)
+   .post('/topic/:topicID/edit',topic.edit)
+   .get('/topic/:topicID/delete',topic.delete)
+
 //3.导出路由对象
 module.exports = router
 //4.在app.js中通过app.use(路由对象)挂载使之生效
